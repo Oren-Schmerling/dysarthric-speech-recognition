0cf4d2978d301d77f1cc7cd83e6430f38846d2e9
Oren_Schmerling <orenschmerling@gmail.com>
2025-11-19 00:32:01 -0500

    add conda to job

--- job.sh
+++ job.sh
@@ -9,6 +9,7 @@
 
 # Load necessary modules if required (example for Python)
 module load python/3.11.7  # Adjust or remove based on your environment
+module load conda/latest
 
 # Activate virtual environment and run the job
 cd /home/oschmerling_umass_edu/dysarthric-speech-recognition/
